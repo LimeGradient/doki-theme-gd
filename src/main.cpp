0c90ef2c549fb2b605c4ddd8d7108a58f49771ed
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-04-19 09:50:52 -0400

    fixed bugs and added features :D

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/CCDirector.hpp>
 #include <Geode/modify/LevelEditorLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -16,7 +17,7 @@ std::string getSticker() {
 		case 3:
 			return "kanna_dark.png"_spr;
 		case 4:
-			return "mai_dark_spicy.png"_spr;
+			return "nagaroro_dark.png"_spr;
 		case 5:
 		 	return "mai_dark.png"_spr;
 		case 6:
@@ -34,24 +35,98 @@ std::string getSticker() {
 	}
 }
 
-class $modify(DokiEditorLayer, LevelEditorLayer) {
+std::string getBackground() {
+	auto value = Mod::get()->getSettingValue<int64_t>("BackgroundSlider");
+	switch (value) {
+		case 1:
+			return "aqua_dark.png"_spr;
+		case 2:
+			return "astolfo_dark.png"_spr;
+		case 3:
+			return "asuna_dark.png"_spr;
+		case 4:
+			return "azuki_dark.png"_spr;
+		case 5:
+		 	return "beatrice.png"_spr;
+		case 6:
+			return "cinnamon_dark.png"_spr;
+		case 7:
+			return "coconut_dark.png"_spr;
+		case 8:
+			return "darkness_dark.png"_spr;
+		case 9:
+			return "echidna_dark.png"_spr;
+		case 10:
+			return "gray_dark.png"_spr;
+		default:
+			return "ram.png"_spr;
+	}
+}
+
+class $modify(LevelEditorLayer) {
 	bool init(GJGameLevel* p0, bool p1) {
 		if (!LevelEditorLayer::init(p0, p1)) {
 			return false;
 		}
 
-		auto director = CCDirector::get();
+		auto sceneManager = SceneManager::get();
+		auto scene = CCScene::get();
+
+		if (!Mod::get()->getSettingValue<bool>("ShowStickerInEditor")) {
+			scene->getChildByID("doki-theme-gd")->setVisible(false);
+		}
+
+		if (!Mod::get()->getSettingValue<bool>("ShowBackgroundInEditor")) {
+			return true;
+		}
 
 		auto mainNode = this->getChildByID("EditorUI");
-		auto sticker = CCSprite::createWithSpriteFrameName(getSticker().c_str());
+
+		auto sticker = CCSprite::createWithSpriteFrameName(getBackground().c_str());
 
 		sticker->setPosition(ccp(mainNode->getScaledContentSize().width / 2.f, mainNode->getScaledContentSize().height / 1.5f));
-		sticker->setOpacity(75);
-		sticker->setScale(3.5f);
+		sticker->setOpacity(Mod::get()->getSettingValue<int64_t>("TransparencySliderEditor"));
+		sticker->setScale(0.5f);
 		sticker->setZOrder(-60);
 		sticker->setID("doki-sticker");
 		mainNode->addChild(sticker);
 		return true;
+	}
+
+	void onExit() {
+		auto scene = CCScene::get();
+
+		if (!Mod::get()->getSettingValue<bool>("ShowStickerInEditor")) {
+			scene->getChildByID("doki-theme-gd")->setVisible(true);
+		}
+
+		LevelEditorLayer::onExit();
+	}
+};
+
+class $modify(PlayLayer) {
+	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
+			return false;
+		}
+
+		auto scene = CCScene::get();
+
+		if (!Mod::get()->getSettingValue<bool>("ShowStickerInLevel")) {
+			 scene->getChildByID("doki-theme-gd")->setVisible(false);
+		}
+
+		return true;
+	}
+
+	void onQuit() {
+		auto scene = CCScene::get();
+
+		if (!Mod::get()->getSettingValue<bool>("ShowStickerInLevel")) {
+			 scene->getChildByID("doki-theme-gd")->setVisible(true);
+		}
+
+		PlayLayer::onQuit();
 	}
 };
 
@@ -72,15 +147,16 @@ class $modify(MenuLayer) {
 
 		auto menu = CCMenu::create();
 		menu->setID("doki-theme-gd");
+		menu->setPosition(ccp(0.f, 0.f));
 
 		auto stickerNode = CCNode::create();
 		stickerNode->setID("sticker-node");
 
 		auto sticker = CCSprite::createWithSpriteFrameName(getSticker().c_str());
-		sticker->setID("doki-sticker");
-		menu->setPosition(ccp(0.f, 0.f));
 		sticker->setPosition(ccp(director->getScreenRight() - 27.f, director->getScreenBottom() + 46.f));
 		sticker->setScale(1.5f);
+		sticker->setOpacity(Mod::get()->getSettingValue<int64_t>("TransparencySliderLevel") * 5);
+		sticker->setID("doki-sticker");
 
 		stickerNode->addChild(sticker);
 		menu->addChild(stickerNode);
